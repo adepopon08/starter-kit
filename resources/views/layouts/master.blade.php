fb4df501a83c1e6191163bf1eff26b6c1e3f82ec
Ade Popon Nurlela <adepoponnurlela@gmail.com>
2023-10-21 11:17:31 +0700

    update nama template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,17 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Favicon icon-->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('templateadmin/assets/images/favicon/favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('template/assets/images/favicon/favicon.ico') }}">
 
     <!-- Libs CSS -->
 
-    <link href="{{ asset('templateadmin/assets/libs/bootstrap-icons/font/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('templateadmin/assets/libs/dropzone/dist/dropzone.css') }}" rel="stylesheet">
-    <link href="{{ asset('templateadmin/assets/libs/@mdi/font/css/materialdesignicons.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('templateadmin/assets/libs/prismjs/themes/prism-okaidia.css') }}" rel="stylesheet">
+    <link href="{{ asset('template/assets/libs/bootstrap-icons/font/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('template/assets/libs/dropzone/dist/dropzone.css') }}" rel="stylesheet">
+    <link href="{{ asset('template/assets/libs/@mdi/font/css/materialdesignicons.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('template/assets/libs/prismjs/themes/prism-okaidia.css') }}" rel="stylesheet">
 
     <!-- Theme CSS -->
-    <link rel="stylesheet" href="{{ asset('templateadmin/assets/css/theme.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('template/assets/css/theme.min.css') }}">
     <title>Homepage | Dash Ui - Bootstrap 5 Admin Dashboard Template</title>
 </head>
 
@@ -43,19 +43,18 @@
 
     <!-- Scripts -->
     <!-- Libs JS -->
-    <script src="{{ asset('templateadmin/assets/libs/jquery/dist/jquery.min.js') }}"></script>
-    <script src="{{ asset('templateadmin/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('templateadmin/assets/libs/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
-    <script src="{{ asset('templateadmin/assets/libs/feather-icons/dist/feather.min.js') }}"></script>
-    <script src="{{ asset('templateadmin/assets/libs/prismjs/prism.js') }}"></script>
-    <script src="{{ asset('templateadmin/assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
-    <script src="{{ asset('templateadmin/assets/libs/dropzone/dist/min/dropzone.min.js') }}"></script>
-    <script src="{{ asset('templateadmin/assets/libs/prismjs/plugins/toolbar/prism-toolbar.min.js') }}"></script>
-    <script
-        src="{{ asset('templateadmin/assets/libs/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js') }}">
+    <script src="{{ asset('template/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('template/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('template/assets/libs/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{ asset('template/assets/libs/feather-icons/dist/feather.min.js') }}"></script>
+    <script src="{{ asset('template/assets/libs/prismjs/prism.js') }}"></script>
+    <script src="{{ asset('template/assets/libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('template/assets/libs/dropzone/dist/min/dropzone.min.js') }}"></script>
+    <script src="{{ asset('template/assets/libs/prismjs/plugins/toolbar/prism-toolbar.min.js') }}"></script>
+    <script src="{{ asset('template/assets/libs/prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js') }}">
     </script>
     <!-- Theme JS -->
-    <script src="{{ asset('templateadmin/assets/js/theme.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/theme.min.js') }}"></script>
 
 
 
